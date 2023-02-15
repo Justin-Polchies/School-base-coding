fe4af4619ddfef880ff30af13af227e09dcfeb66
Justin Polchies <99366212+JPolchies@users.noreply.github.com>
2023-02-15 15:40:57 -0500

    some slight changes

--- argsort.c
+++ argsort.c
@@ -9,14 +9,20 @@ int main(int argc, char* argv[])
     int a = 0, i = 0;
     char* charString[argc - 1];
 
+    if (argc < 2) {
+        printf("%s", "No string entered on the Command line.\n");
+        return 0;
+    }
+    //fill a Char array with the argv values from the command line.
     for (int i = 1; i < argc; i++) {
         charString[i - 1] = argv[i];
     }
 
+
     int charSize = argc - 1;
     //sort the strings by length
     qsort(charString, charSize, sizeof(char*), compare);
-    
+
     //Print out the results from qsort
     for (a = 0; a < charSize; a++) {
         printf("%s \n", charString[a]);
@@ -26,7 +32,7 @@ int main(int argc, char* argv[])
 
 //to compare the strings and their lengths putting them in order by smaller to larger (length)
 int compare(const void* firstStr, const void* secondStr) {
-    
+
     char* first_str = *(char**)firstStr;
     char* second_str = *(char**)secondStr;
 
@@ -38,5 +44,5 @@ int compare(const void* firstStr, const void* secondStr) {
     }
 
 
-    return strcmp(first_str, *second_str);
+    return strcmp(first_str, second_str);
 }
