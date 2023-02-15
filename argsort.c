48679f3027766046d22374677398ff627157bdb1
Justin Polchies <99366212+JPolchies@users.noreply.github.com>
2023-02-15 15:28:00 -0500

    New File and Updates

---
+++ argsort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+int compare(const void* firstStr, const void* secondStr);
+
+int main(int argc, char* argv[])
+{
+    int a = 0, i = 0;
+    char* charString[argc - 1];
+
+    for (int i = 1; i < argc; i++) {
+        charString[i - 1] = argv[i];
+    }
+
+    int charSize = argc - 1;
+    //sort the strings by length
+    qsort(charString, charSize, sizeof(char*), compare);
+    
+    //Print out the results from qsort
+    for (a = 0; a < charSize; a++) {
+        printf("%s \n", charString[a]);
+    }
+    return 0;
+}
+
+//to compare the strings and their lengths putting them in order by smaller to larger (length)
+int compare(const void* firstStr, const void* secondStr) {
+    
+    char* first_str = *(char**)firstStr;
+    char* second_str = *(char**)secondStr;
+
+    if (strlen(first_str) < strlen(second_str)) {
+        return 0;
+    }
+    if (strlen(first_str) > strlen(second_str)) {
+        return 1;
+    }
+
+
+    return strcmp(first_str, *second_str);
+}
