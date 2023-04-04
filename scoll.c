79f9a58bc377d54834c19a4cc08a4ab30b58620c
Justin Polchies <99366212+JPolchies@users.noreply.github.com>
2023-04-04 12:36:22 -0400

    two new files.

--- scoll.c
+++ scoll.c
@@ -1,11 +1,14 @@
-include <stdio.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 
 int main() {
 
+	fork();
+	fork();
 
-
-
-
+	int pid = getpid();
+	printf("Current PID #: %d\n", pid);
 	return 0;
 }
