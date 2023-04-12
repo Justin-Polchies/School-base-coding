6120d5e5cbee76ab7cefe00b7af4399026c21282
Justin Polchies <99366212+JPolchies@users.noreply.github.com>
2023-04-12 10:44:25 -0400

    new file and updated file

--- scroll.c
+++ scroll.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/time.h>
 
+#define MAXBUF 250000
 
 int main() {
+	char name[125];
 
-	fork();
-	fork();
+	fileOpener();
 
-	int pid = getpid();
-	printf("Current PID #: %d\n", pid);
 	return 0;
+}
+
+void fileOpener() {
+
+	FILE* fp;
+	char* buf = 0;
+	int filesize;
+
+	if (fp) {
+
+		fseek(fp, 0, SEEK_END);
+		filesize = ftell(fp);
+		printf("%d", filesize);
+
+		buf = malloc(filesize);
+		if (buf) {
+			fread(buf, 1, filesize, fp);
+
+		}
+		printf(" %.*s", len, buf);
+
+		fclose(fp)
+
+	}
+
 }
